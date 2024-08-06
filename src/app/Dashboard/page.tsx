45aca5feff3289a40efafa592799e24d5153c8dc
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-08-06 15:37:55 +0530

    custom grid added

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      Dashboard
+    </div>
+  );
 };
 
 export default Dashboard;
